631e9a9424c9c7b2c163044e012ab29b243b3eed
Ebo Sompa Dennis <35781440+ebosompa@users.noreply.github.com>
2020-09-12 00:12:31 -0400

    Add xcode initial files

---
+++ Detection Alerts/Detection Alerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Detection Alerts
+//
+//  Created by Ebo Sompa Dennis on 9/11/20.
+//  Copyright © 2020 Ebo Sompa Dennis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
